bb1d94f6dff66a27b6dd82c03589d7b45f9b9361
radifkhanrafin <meherafkabir@gmail.com>
2023-05-20 18:11:19 +0600

    update versel

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ const port = process.env.PORT || 5000
 app.use(cors());
 app.use(express.json());
 
-// const uri = `mongodb+srv://${process.env.DATABASE_USER}:${process.env.DATABASE_PASS}@toysmarketdata.uv9io2a.mongodb.net/?retryWrites=true&authSource=admin`;
+const uri = `mongodb+srv://${process.env.DATABASE_USER}:${process.env.DATABASE_PASS}@toysmarketdata.uv9io2a.mongodb.net/?retryWrites=true&authSource=admin`;
 
-const uri = 'mongodb+srv://toysMarketData:4phbYiE5yF03LRJB@toysmarketdata.uv9io2a.mongodb.net/?retryWrites=true&authSource=admin';
+// const uri = 'mongodb+srv://toysMarketData:4phbYiE5yF03LRJB@toysmarketdata.uv9io2a.mongodb.net/?retryWrites=true&authSource=admin';
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -25,7 +25,7 @@ async function run() {
     try {
         //  client.connect();
 
-        
+
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
