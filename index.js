24ed4e82862868b45754efa5fe70207e2fc2986f
radifkhanrafin <meherafkabir@gmail.com>
2023-05-20 17:46:50 +0600

    user pass hide

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ const port = process.env.PORT || 5000
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DATABASE_USER}:${process.env.DATABASE_PASS}@toysmarketdata.uv9io2a.mongodb.net/?retryWrites=true&authSource=admin`;
+// const uri = `mongodb+srv://${process.env.DATABASE_USER}:${process.env.DATABASE_PASS}@toysmarketdata.uv9io2a.mongodb.net/?retryWrites=true&authSource=admin`;
+
+const uri = 'mongodb+srv://toysMarketData:4phbYiE5yF03LRJB@toysmarketdata.uv9io2a.mongodb.net/?retryWrites=true&authSource=admin';
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -22,7 +24,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-         client.connect();
+        //  client.connect();
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
